9ee78ef28f91809dec8c776d1c3eceb8b352bbb0
ajsrDai <ajsrDai@gmail.com>
2025-06-25 14:32:31 +0530

    updated test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.datasets import load_diabetes
+import joblib
+import pandas as pd
+
+X,y = load_diabetes(return_X_y=True)
+_, X_test, _, y_test = train_test_split(X, y, test_size=0.2,)
+
+model = joblib.load("model.pkl")
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error on test set: {mse:.2f}")
